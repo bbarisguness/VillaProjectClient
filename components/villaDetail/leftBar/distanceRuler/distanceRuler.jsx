8be87fe14ee40104b6540da5fdcef43a3f5a4ce6
tevfikkdurmus <tevfikk.durmus@gmail.com>
2024-10-17 12:34:40 +0300

    distanceRuler and priceTable edited

--- components/villaDetail/leftBar/distanceRuler/distanceRuler.jsx
+++ components/villaDetail/leftBar/distanceRuler/distanceRuler.jsx
@@ -1,36 +1,48 @@
-import styles from "./distanceRuler.module.css"
-import Link from "next/link"
+import styles from "./distanceRuler.module.css";
+import Link from "next/link";
 
 export default function DistanceRuler({ data }) {
-    return (
-        <>
-            <div className={styles.villaDetailTitle}>Mesafe Cetveli</div>
-            <div className={styles.services}>
-                <div className={styles.box}>
-                    <div className={styles.bottom}>
-                        <div className={styles.row}>
-                            <ul>
-                                {
-                                    data?.map((data, index) => (
-                                        <li key={index}>
-                                            <div className={styles.column}>
-                                                <Link href="#">
-                                                    <div className={styles.iconBox}>
-                                                        <i style={{ backgroundImage: `url(/images/${data?.icon}.png)` }}></i>
-                                                        <i className={styles.two_i} style={{ backgroundImage: `url(/images/${data?.icon}-hover.png)` }}></i>
-                                                    </div>
-                                                    <div className={styles.title}>{data?.distanceRulerDetails[0]?.name}</div>
-                                                    <div className={styles.distance}>{data?.distanceRulerDetails[0]?.value}</div>
-                                                </Link>
-                                            </div>
-                                        </li>
-                                    ))
-                                }
-                            </ul>
+  if (data?.length == 0) return null;
+  return (
+    <>
+      <div className={styles.villaDetailTitle}>Mesafe Cetveli</div>
+      <div className={styles.services}>
+        <div className={styles.box}>
+          <div className={styles.bottom}>
+            <div className={styles.row}>
+              <ul>
+                {data?.map((data, index) => (
+                  <li key={index}>
+                    <div className={styles.column}>
+                      <Link href="#">
+                        <div className={styles.iconBox}>
+                          <i
+                            style={{
+                              backgroundImage: `url(/images/${data?.icon}.png)`,
+                            }}
+                          ></i>
+                          <i
+                            className={styles.two_i}
+                            style={{
+                              backgroundImage: `url(/images/${data?.icon}-hover.png)`,
+                            }}
+                          ></i>
                         </div>
+                        <div className={styles.title}>
+                          {data?.distanceRulerDetails[0]?.name}
+                        </div>
+                        <div className={styles.distance}>
+                          {data?.distanceRulerDetails[0]?.value}
+                        </div>
+                      </Link>
                     </div>
-                </div>
+                  </li>
+                ))}
+              </ul>
             </div>
-        </>
-    )
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- components/villaDetail/leftBar/priceTable/priceTable.jsx
+++ components/villaDetail/leftBar/priceTable/priceTable.jsx
@@ -1,87 +1,135 @@
-'use client';
-import styles from "./priceTable.module.css"
-import React, { useEffect, useState } from "react"
-import Link from "next/link"
+"use client";
+import styles from "./priceTable.module.css";
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
 import { getPriceTypeDetail } from "@/data/data";
 
-
 export default function PriceTable({ data, priceTypeNumber }) {
+  // console.log(data);
 
-    // console.log(data);
+  const [priceTableActiveIndex, setPriceTableActiveIndex] =
+    useState(priceTypeNumber);
 
-    const [priceTableActiveIndex, setPriceTableActiveIndex] = useState(priceTypeNumber)
+  const handlePriceTable = (index) => {
+    setPriceTableActiveIndex(index);
+  };
 
-    const handlePriceTable = (index) => {
-        setPriceTableActiveIndex(index)
+  // exchange
+  const [usd, setUsd] = useState(34.31);
+  const [eur, setEur] = useState(37.56);
+  const [gbp, setGbp] = useState(44.85);
+  // useEffect(() => {
 
+  //     fetch('http://hasanadiguzel.com.tr/api/kurgetir')
+  //         .then(response => response.json())
+  //         .then(data => {
+  //             //console.log(data.TCMB_AnlikKurBilgileri)
+  //             //setExchanges(data.TCMB_AnlikKurBilgileri)
+  //             //setReady(true)
+  //             setUsd(data.TCMB_AnlikKurBilgileri[0].BanknoteSelling)
+  //             setEur(data.TCMB_AnlikKurBilgileri[3].BanknoteSelling)
+  //             setGbp(data.TCMB_AnlikKurBilgileri[4].BanknoteSelling)
+  //         });
 
-    }
+  // }, [gbp])
 
-    // exchange
-    const [usd, setUsd] = useState(34.31)
-    const [eur, setEur] = useState(37.56)
-    const [gbp, setGbp] = useState(44.85)
-    // useEffect(() => {
+  if (data?.length == 0) return null;
 
-    //     fetch('http://hasanadiguzel.com.tr/api/kurgetir')
-    //         .then(response => response.json())
-    //         .then(data => {
-    //             //console.log(data.TCMB_AnlikKurBilgileri)
-    //             //setExchanges(data.TCMB_AnlikKurBilgileri)
-    //             //setReady(true)
-    //             setUsd(data.TCMB_AnlikKurBilgileri[0].BanknoteSelling)
-    //             setEur(data.TCMB_AnlikKurBilgileri[3].BanknoteSelling)
-    //             setGbp(data.TCMB_AnlikKurBilgileri[4].BanknoteSelling)
-    //         });
-
-    // }, [gbp])
-
-    return (
-        <div className={styles.priceTable}>
-            <div className={styles.top}>
-                <div className={styles.title}>Fiyat Tablosu</div>
-                <div className={styles.exchangeRateMenu}>
-                    <ul>
-                        <li onClick={() => handlePriceTable(1)} className={`${priceTableActiveIndex == 1 ? styles["active"] : ""}`}><Link onClick={(e) => e.preventDefault()} href="#">TL</Link></li>
-                        <li onClick={() => handlePriceTable(2)} className={`${priceTableActiveIndex == 2 ? styles["active"] : ""}`}><Link onClick={(e) => e.preventDefault()} href="#">DOLAR</Link></li>
-                        <li onClick={() => handlePriceTable(3)} className={`${priceTableActiveIndex == 3 ? styles["active"] : ""}`}><Link onClick={(e) => e.preventDefault()} href="#">EURO</Link></li>
-                        <li onClick={() => handlePriceTable(4)} className={`${priceTableActiveIndex == 4 ? styles["active"] : ""}`}><Link onClick={(e) => e.preventDefault()} href="#">POUND</Link></li>
-                    </ul>
+  return (
+    <div className={styles.priceTable}>
+      <div className={styles.top}>
+        <div className={styles.title}>Fiyat Tablosu</div>
+        <div className={styles.exchangeRateMenu}>
+          <ul>
+            <li
+              onClick={() => handlePriceTable(1)}
+              className={`${
+                priceTableActiveIndex == 1 ? styles["active"] : ""
+              }`}
+            >
+              <Link onClick={(e) => e.preventDefault()} href="#">
+                TL
+              </Link>
+            </li>
+            <li
+              onClick={() => handlePriceTable(2)}
+              className={`${
+                priceTableActiveIndex == 2 ? styles["active"] : ""
+              }`}
+            >
+              <Link onClick={(e) => e.preventDefault()} href="#">
+                DOLAR
+              </Link>
+            </li>
+            <li
+              onClick={() => handlePriceTable(3)}
+              className={`${
+                priceTableActiveIndex == 3 ? styles["active"] : ""
+              }`}
+            >
+              <Link onClick={(e) => e.preventDefault()} href="#">
+                EURO
+              </Link>
+            </li>
+            <li
+              onClick={() => handlePriceTable(4)}
+              className={`${
+                priceTableActiveIndex == 4 ? styles["active"] : ""
+              }`}
+            >
+              <Link onClick={(e) => e.preventDefault()} href="#">
+                POUND
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className={styles.bottom}>
+        <ul>
+          {data?.map((data, index) => (
+            <li key={index}>
+              <div className={styles.box}>
+                <div className={styles.leftBox}>
+                  <div className={styles.imageBox}>
+                    <i className={styles.cloud_icon}></i>
+                  </div>
                 </div>
-            </div>
-            <div className={styles.bottom}>
-                <ul>
-
-                    {
-                        data?.map((data, index) => (
-                            <li key={index}>
-                                <div className={styles.box}>
-                                    <div className={styles.leftBox}>
-                                        <div className={styles.imageBox}><i className={styles.cloud_icon}></i></div>
-                                    </div>
-                                    <div className={styles.rightBox}>
-                                        <div className={styles.name}>
-                                            {data?.priceTableDetails[0]?.title}
-                                        </div>
-                                        <div className={styles.desc}>{data?.priceTableDetails[0]?.description}</div>
-                                        <div className={styles.price}>
-                                            {/* {
+                <div className={styles.rightBox}>
+                  <div className={styles.name}>
+                    {data?.priceTableDetails[0]?.title}
+                  </div>
+                  <div className={styles.desc}>
+                    {data?.priceTableDetails[0]?.description}
+                  </div>
+                  <div className={styles.price}>
+                    {/* {
                                                 (priceTableActiveIndex === 2 ? ((data?.price / usd).toFixed(2) + " " + getPriceTypeDetail(2)?.text) : priceTableActiveIndex === 3 ? ((data?.price / eur).toFixed(2) + " " + getPriceTypeDetail(3)?.text) : priceTableActiveIndex === 4 ? ((data?.price / gbp).toFixed(2) + " " + getPriceTypeDetail(4)?.text) : (data?.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") + " " + getPriceTypeDetail(1)?.text))
                                             } */}
-                                            {
-                                                (priceTableActiveIndex === 2 ? ((data?.price).toFixed(2) + " " + getPriceTypeDetail(2)?.text) : priceTableActiveIndex === 3 ? ((data?.price).toFixed(2) + " " + getPriceTypeDetail(3)?.text) : priceTableActiveIndex === 4 ? ((data?.price).toFixed(2) + " " + getPriceTypeDetail(4)?.text) : (data?.price.toFixed(2) + " " + getPriceTypeDetail(1)?.text))
-                                            }
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                        ))
-                    }
-
-
-                </ul>
-                <p className={styles.notice}>Özel günlerde (Bayram, Yılbaşı, vb..) fiyat değişiklik göstermektedir.</p>
-            </div>
-        </div>
-    )
+                    {priceTableActiveIndex === 2
+                      ? (data?.price).toFixed(2) +
+                        " " +
+                        getPriceTypeDetail(2)?.text
+                      : priceTableActiveIndex === 3
+                      ? (data?.price).toFixed(2) +
+                        " " +
+                        getPriceTypeDetail(3)?.text
+                      : priceTableActiveIndex === 4
+                      ? (data?.price).toFixed(2) +
+                        " " +
+                        getPriceTypeDetail(4)?.text
+                      : data?.price.toFixed(2) +
+                        " " +
+                        getPriceTypeDetail(1)?.text}
+                  </div>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <p className={styles.notice}>
+          Özel günlerde (Bayram, Yılbaşı, vb..) fiyat değişiklik göstermektedir.
+        </p>
+      </div>
+    </div>
+  );
 }
