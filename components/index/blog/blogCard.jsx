06f0973d3fc972b9236f47b3f60c9ec68cf0ad72
tevfikkdurmus <tevfikk.durmus@gmail.com>
2024-10-19 16:29:22 +0300

    blog component shortText added

--- components/index/blog/blogCard.jsx
+++ components/index/blog/blogCard.jsx
@@ -1,110 +1,114 @@
-import styles from "./blogCard.module.css"
-import Link from "next/link"
+import styles from "./blogCard.module.css";
+import Link from "next/link";
 
 export default function BlogCard({ data }) {
-    // const generateBlogLink = (title) => {
-    //     let url = title
-    //         .toLowerCase()
-    //         .replaceAll(" ", "-")
-    //         .replaceAll("?", "")
-    //         .replaceAll(":", "")
-    //         .replaceAll("ı", "i")
-    //         .replaceAll("ç", "c")
-    //         .replaceAll("ş", "s")
-    //         .replaceAll("ö", "o")
-    //         .replaceAll("ü", "u")
-    //         .replaceAll("ğ", "g")
-    //         .replaceAll("ı", "i")
-    //     return url
-    // }
+  console.log(data);
+  // const generateBlogLink = (title) => {
+  //     let url = title
+  //         .toLowerCase()
+  //         .replaceAll(" ", "-")
+  //         .replaceAll("?", "")
+  //         .replaceAll(":", "")
+  //         .replaceAll("ı", "i")
+  //         .replaceAll("ç", "c")
+  //         .replaceAll("ş", "s")
+  //         .replaceAll("ö", "o")
+  //         .replaceAll("ü", "u")
+  //         .replaceAll("ğ", "g")
+  //         .replaceAll("ı", "i")
+  //     return url
+  // }
 
-    const formatDate = (newDateString) => {
-        var originalDateString = newDateString;
+  const formatDate = (newDateString) => {
+    var originalDateString = newDateString;
 
-        var parts = originalDateString.split('.');
-        var day = parseInt(parts[0], 10).toString();
-        var month = parseInt(parts[1], 10).toString();
-        var year = parts[2];
+    var parts = originalDateString.split(".");
+    var day = parseInt(parts[0], 10).toString();
+    var month = parseInt(parts[1], 10).toString();
+    var year = parts[2];
 
-        // Gün ve ayı kontrol edip başındaki sıfırları kaldırın
-        day = day.replace(/^0+/, '');
-        month = month.replace(/^0+/, '');
+    // Gün ve ayı kontrol edip başındaki sıfırları kaldırın
+    day = day.replace(/^0+/, "");
+    month = month.replace(/^0+/, "");
 
-        var newDateString = day + '.' + month + '.' + year;
+    var newDateString = day + "." + month + "." + year;
 
-        return newDateString
-    }
+    return newDateString;
+  };
 
-    const formatTime = (hour) => {
-        var timeString = hour;
+  const formatTime = (hour) => {
+    var timeString = hour;
 
-        // Bugünün tarihini alın
-        var today = new Date();
+    // Bugünün tarihini alın
+    var today = new Date();
 
-        // Saati, dakikayı ve saniyeyi alın
-        var hours = today.getUTCHours();
-        var minutes = today.getUTCMinutes();
-        var seconds = today.getUTCSeconds();
+    // Saati, dakikayı ve saniyeyi alın
+    var hours = today.getUTCHours();
+    var minutes = today.getUTCMinutes();
+    var seconds = today.getUTCSeconds();
 
-        // Saati, dakikayı ve saniyeyi 'timeString' tarih dizgisinden alın
-        var timeParts = timeString.split(/[:.Z]/);
-        hours = parseInt(timeParts[0], 10);
-        minutes = parseInt(timeParts[1], 10);
-        seconds = parseInt(timeParts[2], 10);
+    // Saati, dakikayı ve saniyeyi 'timeString' tarih dizgisinden alın
+    var timeParts = timeString.split(/[:.Z]/);
+    hours = parseInt(timeParts[0], 10);
+    minutes = parseInt(timeParts[1], 10);
+    seconds = parseInt(timeParts[2], 10);
 
-        // Yeni Date objesini oluşturun ve saat, dakika ve saniyeyi ayarlayın
-        var newDate = new Date();
-        newDate.setUTCHours(hours);
-        newDate.setUTCMinutes(minutes);
-        newDate.setUTCSeconds(seconds);
+    // Yeni Date objesini oluşturun ve saat, dakika ve saniyeyi ayarlayın
+    var newDate = new Date();
+    newDate.setUTCHours(hours);
+    newDate.setUTCMinutes(minutes);
+    newDate.setUTCSeconds(seconds);
 
-        // Yeni saat dizgisini oluşturun
-        var newTimeString = newDate.toISOString().substr(11, 8);
+    // Yeni saat dizgisini oluşturun
+    var newTimeString = newDate.toISOString().substr(11, 8);
 
-        return newTimeString; // Çıktı: "12:03:22"
-    }
+    return newTimeString; // Çıktı: "12:03:22"
+  };
 
-    const formatDateAndTime = () => {
-        const _data = data?.attributes?.createdAt?.split('T')
-        const _date = _data[0]
-        const _hour = _data[1]
+  const formatDateAndTime = () => {
+    const _data = data?.attributes?.createdAt?.split("T");
+    const _date = _data[0];
+    const _hour = _data[1];
 
-        var originalDateString = _date;
-        var originalDate = new Date(originalDateString);
-        var newDateString = originalDate.toLocaleDateString("tr-TR"); // day.month.year 13.01.2024
+    var originalDateString = _date;
+    var originalDate = new Date(originalDateString);
+    var newDateString = originalDate.toLocaleDateString("tr-TR"); // day.month.year 13.01.2024
 
-        const formatedData = formatDate(newDateString) + ' ' + formatTime(_hour)
+    const formatedData = formatDate(newDateString) + " " + formatTime(_hour);
 
-        return formatedData
-    }
+    return formatedData;
+  };
 
-    return (
-        <li className={styles.blogCardContainer}>
-            <Link href={`bloglar/${data.id}`}>
-                <div className={styles.imgBox}>
-                    <div className={styles.bgImage}
-                        style={{ backgroundImage: `url(https://villaapi.testgrande.com/Uploads/WebPhotos/k_${data?.photos[0]?.image})` }}>
-                    </div>
-                    <div className={styles.cardFeatures}>Seyahat Uzmanı</div>
-                </div>
-                <div className={styles.textBox}>
-                    <div className={styles.features}>
-                        <div className={styles.colon}>
-                            <i className={styles.person_icon}></i>
-                            <span>Seyahat Uzmanı</span>
-                        </div>
-                        <div className={styles.colon}>
-                            <i className={styles.date_icon}></i>
-                            <span>1</span>
-                        </div>
-                    </div>
-                    <div className={styles.title}>{data?.webPageDetails[0].title}</div>
-                    <div className={styles.desc}>
-                        {data?.attributes?.descriptionShort}
-                    </div>
-                    <div className={styles.detailButton}>DEVAMI...</div>
-                </div>
-            </Link>
-        </li>
-    )
+  return (
+    <li className={styles.blogCardContainer}>
+      <Link href={`bloglar/${data.id}`}>
+        <div className={styles.imgBox}>
+          <div
+            className={styles.bgImage}
+            style={{
+              backgroundImage: `url(https://villaapi.testgrande.com/Uploads/WebPhotos/k_${data?.photos[0]?.image})`,
+            }}
+          ></div>
+          <div className={styles.cardFeatures}>Seyahat Uzmanı</div>
+        </div>
+        <div className={styles.textBox}>
+          <div className={styles.features}>
+            <div className={styles.colon}>
+              <i className={styles.person_icon}></i>
+              <span>Seyahat Uzmanı</span>
+            </div>
+            <div className={styles.colon}>
+              <i className={styles.date_icon}></i>
+              <span>1</span>
+            </div>
+          </div>
+          <div className={styles.title}>{data?.webPageDetails[0].title}</div>
+          <div className={styles.desc}>
+            {data?.webPageDetails[0]?.descriptionShort}
+          </div>
+          <div className={styles.detailButton}>DEVAMI...</div>
+        </div>
+      </Link>
+    </li>
+  );
 }
