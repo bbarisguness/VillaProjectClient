42cbd5a3015442e90687e6e9ff3b7013c0aba5eb
tevfikkdurmus <tevfikk.durmus@gmail.com>
2024-10-26 12:45:29 +0300

    loading removed

--- pages/_app.js
+++ pages/_app.js
@@ -5,17 +5,18 @@ import Footer from "@/components/footer/footer"
 import HamburgerMenu from "@/components/hamburger/hamburgerMenu"
 
 import "@/styles/styles.css"
-import { usePageLoading } from "@/hooks/usePageLoading"
+//import { usePageLoading } from "@/hooks/usePageLoading"
 import Loading from "@/app/loading"
 
 function myApp({ Component, pageProps }) {
-    const { isPageLoading } = usePageLoading();
+    //const { isPageLoading } = usePageLoading();
     return (
         <Providers store={store}>
             <Header />
-            {isPageLoading ? <Loading /> :
+            {/* {isPageLoading ? <Loading /> :
                 <Component {...pageProps} />
-            }
+            } */}
+            <Component {...pageProps} />
             <Footer />
             <HamburgerMenu />
         </Providers>
