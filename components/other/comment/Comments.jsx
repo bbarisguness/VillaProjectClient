a9b2b019e3483f8bb8ba045bda5973c3607eba58
tevfikkdurmus <tevfikk.durmus@gmail.com>
2024-10-31 16:51:41 +0300

    fixed comments component

--- components/other/comment/Comments.jsx
+++ components/other/comment/Comments.jsx
@@ -23,121 +23,127 @@ export default function Comments({ commentData }) {
     );
   };
 
-  return (
-    <>
-      {/* <div className={styles.title}>4,91 · {commentData?.length} değerlendirme</div>
-      <div className={styles.commentRating}>
-        <ul>
-          <li>
-            <div className={styles.name}>Temizlik</div>
-            <div className={styles.rating}>
-              <div className={styles.graphic}>
-                <span></span>
-              </div>
-              <div className={styles.rate}>1.8</div>
-            </div>
-          </li>
-          <li>
-            <div className={styles.name}>İletişim</div>
-            <div className={styles.rating}>
-              <div className={styles.graphic}>
-                <span></span>
-              </div>
-              <div className={styles.rate}>2.5</div>
-            </div>
-          </li>
-          <li>
-            <div className={styles.name}>Giriş</div>
-            <div className={styles.rating}>
-              <div className={styles.graphic}>
-                <span></span>
-              </div>
-              <div className={styles.rate}>5.0</div>
-            </div>
-          </li>
-          <li>
-            <div className={styles.name}>Doğruluk</div>
-            <div className={styles.rating}>
-              <div className={styles.graphic}>
-                <span></span>
-              </div>
-              <div className={styles.rate}>4.7</div>
-            </div>
-          </li>
-          <li>
-            <div className={styles.name}>Konum</div>
-            <div className={styles.rating}>
-              <div className={styles.graphic}>
-                <span></span>
-              </div>
-              <div className={styles.rate}>3.9</div>
-            </div>
-          </li>
-          <li>
-            <div className={styles.name}>Kalite/fiyat oranı</div>
-            <div className={styles.rating}>
-              <div className={styles.graphic}>
-                <span></span>
-              </div>
-              <div className={styles.rate}>4.7</div>
-            </div>
-          </li>
-        </ul>
-      </div> */}
-      <div className={styles.comments}>
-        <div
-          className={styles.title}
-          style={{ textAlign: "center", fontSize: 26 }}
-        >
-          Yorumlar
-        </div>
-        <ul>
-          {commentData?.slice(0, numberOfCommentsShown).map((item, index) => {
-            return (
-              <li key={"customerComment" + index + 1}>
-                <div className={styles.imageBox}>
-                  <div className={styles.img}>
-                    <img src="/images/person-3.png" alt="" />
-                  </div>
-                  {item?.video != null ? <VideoWithComment /> : null}
+  if (commentData?.length > 0) {
+    return (
+      <>
+        {/* <div className={styles.title}>4,91 · {commentData?.length} değerlendirme</div>
+        <div className={styles.commentRating}>
+          <ul>
+            <li>
+              <div className={styles.name}>Temizlik</div>
+              <div className={styles.rating}>
+                <div className={styles.graphic}>
+                  <span></span>
                 </div>
-                <div className={styles.name}>
-                  {item.name} {item.surName}
+                <div className={styles.rate}>1.8</div>
+              </div>
+            </li>
+            <li>
+              <div className={styles.name}>İletişim</div>
+              <div className={styles.rating}>
+                <div className={styles.graphic}>
+                  <span></span>
                 </div>
-                <div className={styles.dateandRating}>
-                  <div className={styles.date}>
-                    {formatDate(item?.createdAt)}
-                  </div>
-                  <div className={styles.stars}>
-                    <div className={styles.starItems}>
-                      <Star rating={item?.rating} />
-                    </div>
-                    <div className={styles.text}>
-                      ({Math.round(item?.rating)} reviews)
-                    </div>
-                  </div>
+                <div className={styles.rate}>2.5</div>
+              </div>
+            </li>
+            <li>
+              <div className={styles.name}>Giriş</div>
+              <div className={styles.rating}>
+                <div className={styles.graphic}>
+                  <span></span>
                 </div>
-                <div className={styles.descBox}>{item?.commentText}</div>
-              </li>
-            );
-          })}
-        </ul>
-        {!isShowingAllComments && (
-          <div className={styles.linkBox}>
-            <a
-              href="#"
-              onClick={(e) => {
-                e.preventDefault();
-                setShowingAllComments(true);
-                setNumberOfCommentsShown(commentData?.length);
-              }}
-              className={styles.blueButtonArrowOpa}
-            >
-              <span>{commentData?.length} değerlendirmenin tümünü göster</span>
-            </a>
+                <div className={styles.rate}>5.0</div>
+              </div>
+            </li>
+            <li>
+              <div className={styles.name}>Doğruluk</div>
+              <div className={styles.rating}>
+                <div className={styles.graphic}>
+                  <span></span>
+                </div>
+                <div className={styles.rate}>4.7</div>
+              </div>
+            </li>
+            <li>
+              <div className={styles.name}>Konum</div>
+              <div className={styles.rating}>
+                <div className={styles.graphic}>
+                  <span></span>
+                </div>
+                <div className={styles.rate}>3.9</div>
+              </div>
+            </li>
+            <li>
+              <div className={styles.name}>Kalite/fiyat oranı</div>
+              <div className={styles.rating}>
+                <div className={styles.graphic}>
+                  <span></span>
+                </div>
+                <div className={styles.rate}>4.7</div>
+              </div>
+            </li>
+          </ul>
+        </div> */}
+        <div className={styles.comments}>
+          <div
+            className={styles.title}
+            style={{ textAlign: "center", fontSize: 26 }}
+          >
+            Yorumlar
           </div>
-        )}
-      </div>
-    </>
-  );
+          <ul>
+            {commentData?.slice(0, numberOfCommentsShown).map((item, index) => {
+              return (
+                <li key={"customerComment" + index + 1}>
+                  <div className={styles.imageBox}>
+                    <div className={styles.img}>
+                      <img src="/images/person-3.png" alt="" />
+                    </div>
+                    {item?.video != null ? <VideoWithComment /> : null}
+                  </div>
+                  <div className={styles.name}>
+                    {item.name} {item.surName}
+                  </div>
+                  <div className={styles.dateandRating}>
+                    <div className={styles.date}>
+                      {formatDate(item?.createdAt)}
+                    </div>
+                    <div className={styles.stars}>
+                      <div className={styles.starItems}>
+                        <Star rating={item?.rating} />
+                      </div>
+                      <div className={styles.text}>
+                        ({Math.round(item?.rating)} reviews)
+                      </div>
+                    </div>
+                  </div>
+                  <div className={styles.descBox}>{item?.commentText}</div>
+                </li>
+              );
+            })}
+          </ul>
+          {!isShowingAllComments && (
+            <div className={styles.linkBox}>
+              <a
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setShowingAllComments(true);
+                  setNumberOfCommentsShown(commentData?.length);
+                }}
+                className={styles.blueButtonArrowOpa}
+              >
+                <span>
+                  {commentData?.length} değerlendirmenin tümünü göster
+                </span>
+              </a>
+            </div>
+          )}
+        </div>
+      </>
+    );
+  } else {
+    return null;
+  }
 }
