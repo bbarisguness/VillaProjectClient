5f24ff95a622c2781b3a84c05143668088c09ede
tevfikkdurmus <tevfikk.durmus@gmail.com>
2024-11-18 17:04:05 +0300

    added analytcs script

--- pages/_app.js
+++ pages/_app.js
@@ -16,6 +16,21 @@ function myApp({ Component, pageProps }) {
     const [footerData, setFooterData] = useState([]);
 
     useEffect(() => {
+
+        const script = document.createElement('script');
+        script.async = true;
+        script.src = "https://www.googletagmanager.com/gtag/js?id=G-X1BE592FGZ";
+        document.head.appendChild(script);
+
+        const inlineScript = document.createElement('script');
+        inlineScript.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-X1BE592FGZ');
+    `;
+        document.head.appendChild(inlineScript);
+
         async function fetchData() {
             const data = await getRegions()
             setFooterData(data?.data);
