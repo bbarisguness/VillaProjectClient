551161e5f49ec857b0d6713f630f5d217de976f8
tevfikkdurmus <tevfikk.durmus@gmail.com>
2024-12-10 11:54:22 +0300

    fixbugfix

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,31 +1,30 @@
 import Slider from "@/components/index/slider/slider";
-// import TreeStep from "@/components/index/treestep/treestep";
-// import Villa from "@/components/index/villa/villa";
+import TreeStep from "@/components/index/treestep/treestep";
+import Villa from "@/components/index/villa/villa";
+import Activates from "@/components/index/aktiviteler/activate";
 // import Regions from "@/components/index/region/region";
-// import Apart from "@/components/index/apart/apart";
-// import Service from "@/components/index/service/service";
-// import Blog from "@/components/index/blog/blog";
-// import VillaRent from "@/components/index/villaRentInfo/villaRentInfo";
+import Apart from "@/components/index/apart/apart";
+import Service from "@/components/index/service/service";
+import Blog from "@/components/index/blog/blog";
+import VillaRent from "@/components/index/villaRentInfo/villaRentInfo";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { useRouter } from "next/router";
 
 //const Slider = lazy(() => import('@/components/index/slider/slider'));
-const TreeStep = lazy(() => import("@/components/index/treestep/treestep"));
-const Villa = lazy(() => import("@/components/index/villa/villa"));
-const Regions = lazy(() => import("@/components/index/region/region"));
-const Activates = lazy(() => import("@/components/index/aktiviteler/activate"));
-const Apart = lazy(() => import("@/components/index/apart/apart"));
-const Service = lazy(() => import("@/components/index/service/service"));
-const Blog = lazy(() => import("@/components/index/blog/blog"));
-const VillaRent = lazy(() =>
-  import("@/components/index/villaRentInfo/villaRentInfo")
-);
+//const TreeStep = lazy(() => import("@/components/index/treestep/treestep"));
+//const Villa = lazy(() => import("@/components/index/villa/villa"));
+//const Regions = lazy(() => import("@/components/index/region/region"));
+//const Activates = lazy(() => import("@/components/index/aktiviteler/activate"));
+//const Apart = lazy(() => import("@/components/index/apart/apart"));
+//const Service = lazy(() => import("@/components/index/service/service"));
+//const Blog = lazy(() => import("@/components/index/blog/blog"));
+//const VillaRent = lazy(() => import("@/components/index/villaRentInfo/villaRentInfo"));
+
 import { getVillasHome, getHotels } from "@/services/villa";
 import { getCategoriesHome } from "@/services/category";
 import Seo from "@/components/seo";
-import { getRegions } from "@/services/region";
+//import { getRegions } from "@/services/region";
 import { getBlogs } from "@/services/blog";
-import { lazy, Suspense } from "react";
+//import { lazy, Suspense } from "react";
 import { getActivates } from "@/services/activite";
 
 export default function Home({
@@ -38,8 +37,6 @@ export default function Home({
   aparts,
   activates,
 }) {
-  const router = useRouter();
-  const { locale, locales, defaultLocale } = router;
   return (
     <>
       <Seo
@@ -48,20 +45,18 @@ export default function Home({
       />
 
       <Slider />
-      <Suspense fallback={<div>loading</div>}>
-        <section id="contentContainer">
-          <TreeStep />
-          <Villa category={categories} villas={villa} />
-          {/* <Regions homePage={true} regions={regions} /> */}
-          <Activates homePage={true} activates={activates} />
-          <Apart aparts={aparts} />
-          <Service />
-          {/* <NewVillas villas={newVillas} /> */}
-          {/* <Testimonial testimonials={testimonials} /> */}
-          <Blog blog={blogs} />
-          <VillaRent />
-        </section>
-      </Suspense>
+      <section id="contentContainer">
+        <TreeStep />
+        <Villa category={categories} villas={villa} />
+        {/* <Regions homePage={true} regions={regions} /> */}
+        <Activates homePage={true} activates={activates} />
+        <Apart aparts={aparts} />
+        <Service />
+        {/* <NewVillas villas={newVillas} /> */}
+        {/* <Testimonial testimonials={testimonials} /> */}
+        <Blog blog={blogs} />
+        <VillaRent />
+      </section>
     </>
   );
 }
