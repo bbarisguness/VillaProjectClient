28e53d94a995406ad0b3d042ff6bfced0d15f25b
tevfikkdurmus <tevfikk.durmus@gmail.com>
2024-12-24 13:18:37 +0300

    middleware lang system updated

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
- 
+
 const PUBLIC_FILE = /\.(.*)$/
- 
+
 export async function middleware(req: NextRequest) {
   if (
     req.nextUrl.pathname.startsWith('/_next') ||
@@ -10,10 +10,13 @@ export async function middleware(req: NextRequest) {
   ) {
     return
   }
- 
+
+  // Tarayıcı dilini al ve sadece ilk kısmını (örneğin: 'tr') al
+  const browserLanguage = req.headers.get('accept-language')?.split(',')[0].split('-')[0] || 'tr'
+
   if (req.nextUrl.locale === 'default') {
-    const locale = req.cookies.get('NEXT_LOCALE')?.value || 'tr'
- 
+    const locale = req.cookies.get('NEXT_LOCALE')?.value || browserLanguage
+
     return NextResponse.redirect(
       new URL(`/${locale}${req.nextUrl.pathname}${req.nextUrl.search}`, req.url)
     )
