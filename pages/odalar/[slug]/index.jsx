f9c851600ba7a6fc23cb2f87319294e23efc5e3e
tevfikkdurmus <tevfikk.durmus@gmail.com>
2024-12-25 13:52:10 +0300

    removed commentForm from room detail page

--- pages/odalar/[slug]/index.jsx
+++ pages/odalar/[slug]/index.jsx
@@ -193,14 +193,14 @@ export default function List({
               </div>
             </div>
           )}
-          <div className={styles.customerCommentsBox}>
+          {/* <div className={styles.customerCommentsBox}>
             <div className={styles.container}>
               <div className={styles.customerComments}>
-                {/* <Comments /> */}
+                <Comments />
                 <CommentForm t={t} />
               </div>
             </div>
-          </div>
+          </div> */}
           {/* {nearVillas?.data?.length > 0 && (
             <div className={styles.apartments}>
               <div className={styles.container}>
